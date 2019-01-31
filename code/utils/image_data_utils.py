688bc9377bcbbe097545233a3f62a91f55972615
Bhuvan <msbhuvan@uw.edu>
2019-01-30 21:16:08 -0800

    Add functions to create amphipod classification data.

--- code/utils/image_data_utils.py
+++ code/utils/image_data_utils.py
@@ -5,11 +5,16 @@ This file contains data utils which can be used for image data manipulation.
 TODO: Specific parts of this script can be run manually for specific use cases.
 """
 
+from scipy.spatial import distance
 from skimage import io
+from skimage import measure
+from skimage.filters import threshold_otsu
 from skimage.transform import resize
 
+import cv2
 import numpy as np
 import os
+import random
 
 
 def resize_images(src_data_dir, dest_data_dir, shape=(256, 256), classification_data=True):
@@ -41,3 +46,89 @@ def resize_images(src_data_dir, dest_data_dir, shape=(256, 256), classification_
         os.makedirs(dest_label_dir_path)
 
         _resize_images_cur_dir(src_label_dir_path, dest_label_dir_path)
+
+
+def center_crop_pad_mask(patch_dir, mask_dir, center_crop_shape=(128, 128)):
+    # Note: Make a copy of data_dir before running this.
+    def center_crop_pad(img, cropx, cropy):
+        y, x = img.shape[:2]
+        startx = x // 2 - (cropx // 2)
+        starty = y // 2 - (cropy // 2)
+        img_cropped = img[starty:starty + cropy, startx:startx + cropx]
+        pad_size = x - cropx
+        img_padded  = cv2.copyMakeBorder(img_cropped,
+                                         top=pad_size,
+                                         bottom=pad_size,
+                                         left=pad_size,
+                                         right=pad_size,
+                                         borderType=cv2.BORDER_CONSTANT,
+                                         value=0)
+        return img_padded
+
+
+    def _should_crop(raw_mask):
+        binary_mask = raw_mask > threshold_otsu(raw_mask / 255)
+        label_image = measure.label(binary_mask)
+        connected_regions = measure.regionprops(label_image)
+        num_connected_regions = len(connected_regions)
+        if num_connected_regions == 0:
+            raise ValueError("No connected region found.")
+        elif num_connected_regions == 1:
+            chosen_region = connected_regions[0]
+        else:
+            y, x = raw_mask.shape[:2]
+            center_coord = (x / 2, y / 2)
+            chosen_region_index = -1
+            min_dist = np.inf
+            for region_index, region in enumerate(connected_regions):
+                cur_centroid = region.centroid
+                cur_dist = distance.euclidean(center_coord, cur_centroid)
+                if cur_dist < min_dist:
+                    min_dist = cur_dist
+                    chosen_region_index = region_index
+
+            chosen_region = connected_regions[chosen_region_index]
+
+        minr, minc, maxr, maxc = chosen_region.bbox
+        bb_length = maxc - minc
+        bb_width = maxr - minr
+        if bb_length < center_crop_shape[1] and bb_width < center_crop_shape[0]:
+            return True
+        else:
+            return False
+
+
+    for img_file in os.listdir(patch_dir):
+        img = io.imread(os.path.join(patch_dir, img_file))
+        mask_file = "%s_mask%s" % os.path.splitext(img_file)
+        mask = io.imread(os.path.join(mask_dir, mask_file))
+        if _should_crop(mask):
+            cropped_img = center_crop_pad(img, center_crop_shape[1], center_crop_shape[0])
+            # It overwrites the patch.
+            io.imsave(os.path.join(patch_dir, img_file), cropped_img)
+
+
+def random_center_crop_pad(patch_dir, center_crop_shape=(128, 128), prob=0.5):
+    # Note: Make a copy of data_dir before running this.
+    def center_crop_pad(img, cropx, cropy):
+        y, x = img.shape[:2]
+        startx = x // 2 - (cropx // 2)
+        starty = y // 2 - (cropy // 2)
+        img_cropped = img[starty:starty + cropy, startx:startx + cropx]
+        pad_size = x - cropx
+        img_padded  = cv2.copyMakeBorder(img_cropped,
+                                         top=pad_size,
+                                         bottom=pad_size,
+                                         left=pad_size,
+                                         right=pad_size,
+                                         borderType=cv2.BORDER_CONSTANT,
+                                         value=0)
+        return img_padded
+
+
+    for img_file in os.listdir(patch_dir):
+        img = io.imread(os.path.join(patch_dir, img_file))
+        if random.random() < prob:
+            cropped_img = center_crop_pad(img, center_crop_shape[1], center_crop_shape[0])
+            new_img_name = "%s_cropped%s" % os.path.splitext(img_file)
+            io.imsave(os.path.join(patch_dir, new_img_name), cropped_img)
